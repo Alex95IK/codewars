69d72f5a0591f893ba4724df8b86ac46b4504af1
Alex95IK <alex95i.k.7@gmail.com>
2022-03-20 17:20:08 +0200

    finish to last Kata task

--- Kata__Counting_Duplicates.py
+++ Kata__Counting_Duplicates.py
@@ -1,6 +1,23 @@
-def function(n):
-    x = [i for i in n.lower() if i in n.lower()]
-    return x
+# Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits
+# that occur more than once in the input string. The input string can be assumed to contain only alphabets
+# (both uppercase and lowercase) and numeric digits.
+
+def duplicate_count(text):
+    x = text.lower()
+    count = 0
+    for i in x:
+        if x.count(i) > 1:
+            x = x.replace(i, '')
+            count += 1
+    return count
 
 
-print(function('12Aw2dad'))
+print(duplicate_count('1129gAwG2ykdG9aYdK'))
+
+
+# Best Practices!!!
+def best_practices(s):
+    return len([c for c in set(s.lower()) if s.lower().count(c) > 1])
+
+
+print(best_practices('1129gAwG2ykdG9aYdK'))
--- Test.py
+++ Test.py
@@ -1,3 +1,10 @@
-a = [4, 3, 2, 1, 10, 14, -14]
-print(list(map(str, a)))
-# ['4', '3', '2', '1', '10', '14', '-14']
+# a = [4, 3, 2, 1, 10, 14, -14]
+# print(list(map(str, a)))
+# # ['4', '3', '2', '1', '10', '14', '-14']
+
+
+str = "this is string example....wow!!!";
+
+sub = "i";
+
+print(str.count(sub))
